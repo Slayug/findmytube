6d595150f319a69bbdaf7c284baa411f8b576d8b
Anilya <alex.loo@protonmail.com>
2021-12-19 22:06:11 +0100

    feat: post result with fullText to elasticSearch

--- extractor.py
+++ extractor.py
@@ -1,30 +1,68 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import JSONFormatter
+from elasticsearch import Elasticsearch
 
 import json
 import sys
-
-if (len(sys.argv) < 2):
-    sys.exit("Need videoID argument")
-
+import functools
 
 formatter = JSONFormatter()
 
+TRANSCRIPT_INDEX = "transcript"
+
+
+if (len(sys.argv) < 4):
+    sys.exit("Need {videoID} {elasticsearch_host} {elasticsearch_port} arguments")
+
+# define function and stuff
+
+def createIndex(es):
+    try:
+        # Ignore 400 means to ignore "Index Already Exist" error.
+        es.indices.create(index=TRANSCRIPT_INDEX, ignore=400)
+    except Exception as ex:
+        print(str(ex))
+
 
 video_id=sys.argv[1]
+elasticsearch_host=str(sys.argv[2])
+elasticsearch_port=str(sys.argv[3])
+
+es_client = Elasticsearch(f"http://{elasticsearch_host}:{elasticsearch_port}")
+
+if not es_client.ping():
+    sys.exit(f"Cannot connect to elasticsearch server {elasticsearch_host} {elasticsearch_port}")
+
+createIndex(es_client)
+
 transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
 languages={}
 
+
+def format_language(result, _languages, key):
+    _languages[key] = {
+        "fullText": functools.reduce(lambda a, b: f"{a} {b['text']}", result, ""),
+        "translations": formatter.format_transcript(result)
+    }
+
 # iterate over manually created language to scrap them
 for key in transcript_list._manually_created_transcripts:
     language = transcript_list._manually_created_transcripts[key]
-    languages[key] = formatter.format_transcript(language.fetch())
+    
+    format_language(language.fetch(), languages, key)
 
 # iterate over generated to scrap them if they do not exist in the previous manually created
 for key in transcript_list._generated_transcripts:
     if key not in languages:
         language = transcript_list._generated_transcripts[key]
-        languages[key] = formatter.format_transcript(language.fetch())
+        format_language(language.fetch(), languages, key)
         
-print(json.dumps(languages))
+# send result to elastic search
+try:
+    res = es_client.index(index=TRANSCRIPT_INDEX, id=video_id, document=languages)
+    print(res['result'])
+except Exception as ex:
+    print(str(ex))
+#print(json.dumps(languages))
+
