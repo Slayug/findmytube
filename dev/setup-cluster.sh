cec44888c34eaa0e79f6c903d71e37fc92b7c934
Alexandra <alex.loo@protonmail.com>
2022-06-19 00:10:48 +0200

    chore: front charts ok

---
+++ dev/setup-cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+
+cat <<EOF | kind create cluster - -name cluster-fy --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
+
+kub create namespace fy
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+sed -i "127.0.0.1  www.dev.fy.com dev.fy.com api.fy.com" /etc/hosts
