a20d017d703d43ec4f49d530cae1d2b85efe9759
Alexandra <alex.loo@protonmail.com>
2022-06-27 20:34:08 +0200

    chore: rename to findmytube

--- dev/setup-cluster.sh
+++ dev/setup-cluster.sh
@@ -2,10 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-kind create cluster --name cluster-fy --config $DIR/cluster-config.yaml
+kind create cluster --name cluster-findmytube --config $DIR/cluster-config.yaml
 
 kubectl create namespace findmytube
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
-sed -i "127.0.0.1  www.dev.fy.com dev.fy.com api.fy.com" /etc/hosts
+sed -i "127.0.0.1  www.dev.findmytube.io dev.findmytube.io api.findmytube.com" /etc/hosts
