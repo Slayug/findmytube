8f4e4608c94d745e7e9d28deeaa7958c7af64a09
Alexandra <alex.loo@protonmail.com>
2022-07-16 23:17:10 +0200

    chore(api): setup cors

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,7 +4,13 @@ import { Config } from '@findmytube/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: [`https://www.${Config.host}`, `https://${Config.host}`],
+    methods: ['GET', 'POST'],
+  });
+
   await app.listen(Config.apiPort);
 }
+
 bootstrap();
--- packages/core/src/Config.ts
+++ packages/core/src/Config.ts
@@ -2,6 +2,7 @@ const QueueName = process.env.QUEUE_NAME ?? 'videos';
 const ExtractorFileName = 'extractor.py';
 
 const Config = {
+    host: process.env.HOST ?? 'dev.findmytube.io',
     apiHost: process.env.API_HOST ?? 'localhost',
     apiPort: process.env.API_PORT ?? 8080,
     elasticHost: process.env.ELASTIC_HOST ?? 'localhost',
@@ -11,7 +12,7 @@ const Config = {
     elasticChannelIndex: 'channel',
     redisHost: process.env.REDIS_HOST ?? 'localhost',
     redisPort: process.env.REDIS_PORT ?? 6379,
-    redisPassword: process.env.REDIS_PASSWORD ?? "",
+    redisPassword: process.env.REDIS_PASSWORD ?? '',
     videoQueueName: QueueName,
     channelQueueName: 'channel',
     extractorFileName: ExtractorFileName,
