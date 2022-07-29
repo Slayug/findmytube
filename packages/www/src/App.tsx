63861ae993e76f5ea976e25f1495f15c09c5fc57
Alexandra <alex.loo@protonmail.com>
2022-07-29 07:25:24 +0200

    fix(style): scroll list took too much height on mobile

--- packages/www/src/App.tsx
+++ packages/www/src/App.tsx
@@ -34,7 +34,6 @@ function App() {
               <Route path="/watch" element={<VideoPage/>}/>
             </Routes>
           </Content>
-          <Footer>/</Footer>
         </Layout>
       </Router>
 
