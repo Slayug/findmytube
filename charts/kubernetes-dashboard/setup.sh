ac7256f2213dec6b38b110711e4ff0ca5fe3b499
Alexandra <alex.loo@protonmail.com>
2022-08-13 00:34:36 +0200

    ops: enable matrics via values file

--- charts/kubernetes-dashboard/setup.sh
+++ charts/kubernetes-dashboard/setup.sh
@@ -6,6 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Add kubernetes-dashboard repository
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 # Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
-helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard -f $DIR/values.yaml
+
+echo "==============================="
+echo "  SETUP SERVICE ACCOUNT "
+echo "==============================="
 
 kubectl apply -f $DIR/dashboard-adminuser.yaml
