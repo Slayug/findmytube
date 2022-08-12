0882a5f9b5bfd30544d16515097fd2ae06a15fec
Alexandra <alex.loo@protonmail.com>
2022-08-12 23:17:20 +0200

    ops: update elastic values to handle new node

--- charts/elastic/setup.sh
+++ charts/elastic/setup.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 helm repo add elastic https://helm.elastic.co
 
-helm --upgrade install elasticsearch elastic/elasticsearch -f $DIR/values.yaml -n findmytube
+helm upgrade --install elasticsearch elastic/elasticsearch -f $DIR/values.yaml -n findmytube
