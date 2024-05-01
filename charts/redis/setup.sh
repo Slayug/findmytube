cb5cb6a79f342eaa1a84611a03434cb0b941deb0
Alexandra <alex.loo@protonmail.com>
2024-05-01 13:38:29 +0200

    ops: adapt redis with custom storageClassName

--- charts/redis/setup.sh
+++ charts/redis/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-kubectl apply -f $DIR/pvc.yaml
-
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm upgrade --install findmytube-redis bitnami/redis -f $DIR/values.yaml
+helm upgrade --install findmytube-redis-bitnami bitnami/redis -f $DIR/values.yaml
 
 # get redis password
 export REDIS_PASSWORD=$(kubectl get secret --namespace findmytube findmytube-redis -o jsonpath="{.data.redis-password}" | base64 -d)
