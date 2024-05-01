e7026ce937e7bdde9cc1b09fb99c1502595a1a81
Alexandra <alex.loo@protonmail.com>
2024-05-01 11:44:29 +0200

    chore: setup www env file

--- packages/www/src/hooks/useApiVideo.tsx
+++ packages/www/src/hooks/useApiVideo.tsx
@@ -3,7 +3,7 @@ import {VideoResult, SearchVideoResult} from "@findmytube/core";
 import {buildUrlParameters} from "./useApi";
 
 export const VIDEO_ENDPOINT = '/videos';
-axios.defaults.baseURL = `http://localhost:8080`;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export function getVideoById(videoId: string) {
   return axios.get<VideoResult>(`${VIDEO_ENDPOINT}/${videoId}`).then((r) => r.data)
