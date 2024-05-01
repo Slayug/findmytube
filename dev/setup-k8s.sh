de926614535adf6c9db06b74adbc5d5e8c5fedf2
Alexandra <alex.loo@protonmail.com>
2024-05-01 14:04:01 +0200

    ops: adapt elastic with custom storageClassName

--- dev/setup-k8s.sh
+++ dev/setup-k8s.sh
@@ -11,9 +11,12 @@ if [ "$ENV" == "prod" ] || [ "$ENV" == "ovh" ]; then
 else
   $DIR/setup-cluster.sh
 fi
-$DIR/../charts/elastic/setup.sh
 $DIR/../charts/redis/setup.sh
 
+kubectl create -f https://download.elastic.co/downloads/eck/2.11.1/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.11.1/operator.yaml
+
+helm upgrade --install findmytube-elastic ./charts/elastic -f ./charts/values/$ENV.yaml
 helm upgrade --install findmytube-redis ./charts/redis -f ./charts/values/$ENV.yaml
 helm upgrade --install findmytube-issuer ./charts/issuer -f ./charts/values/$ENV.yaml
 helm upgrade --install findmytube-www ./charts/www/ -f ./charts/values/$ENV.yaml
